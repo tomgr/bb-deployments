64c22f698713f44df6c4460de6e91d87550be99a
Thomas Gibson-Robinson <tom@cocotec.io>
2025-08-27 13:41:38 +0000

    experiment

--- tools/run-with-console.py
+++ tools/run-with-console.py
@@ -3,6 +3,17 @@ import sys
 
 result = subprocess.run(
     [sys.executable, "tools/test-interrupt.py"],
-    creationflags=subprocess.CREATE_NEW_PROCESS_GROUP,
+    creationflags=subprocess.CREATE_NEW_CONSOLE|subprocess.CREATE_NEW_PROCESS_GROUP,
+    stdout=subprocess.PIPE,
+    stderr=subprocess.PIPE,
+    startupinfo=subprocess.STARTUPINFO(dwFlags=subprocess.STARTF_USESHOWWINDOW, wShowWindow=subprocess.SW_HIDE),
+    text=True,
 )
+
+if result.stdout:
+    sys.stdout.write(result.stdout)
+    sys.stdout.flush()
+if result.stderr:
+    sys.stderr.write(result.stderr)
+    sys.stderr.flush()
 sys.exit(result.returncode)
